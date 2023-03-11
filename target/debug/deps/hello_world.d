b5a8f0568deec187cc68896c7119fdeb69982f8e
Marcus Lilja <90m.hansson@gmail.com>
2023-03-11 13:25:10 +0100

    Built first hello world program.

---
+++ target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\90mha\OneDrive\Programing\Rust\training\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\90mha\OneDrive\Programing\Rust\training\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Users\90mha\OneDrive\Programing\Rust\training\target\debug\hello-world.exe: C:\Users\90mha\OneDrive\Programing\Rust\training\src\main.rs
